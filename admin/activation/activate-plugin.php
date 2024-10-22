6b1ecbb2e77a9d780cb3a60ff399b527c4fdcc3a
mss-souloftware <sufyan.souloftware@gmail.com>
2024-10-22 15:13:23 +0500

    table setup

--- admin/activation/activate-plugin.php
+++ admin/activation/activate-plugin.php
@@ -18,24 +18,16 @@ function createAllTables(){
            
             $createTablePlugin = "CREATE TABLE $table_plugin  (
                 id int(11) NOT NULL AUTO_INCREMENT,
-                nombre varchar(150) NOT NULL,
-                frase varchar(150) NOT NULL,
+                task varchar(150) NOT NULL,
+                selectedDate date NOT NULL,
+                selectedTime time NOT NULL,
+                userAddress varchar(150) NOT NULL,
                 email varchar(150) NOT NULL,
-                telefono int(11) NOT NULL,
-                cp int(11) NOT NULL,
-                ciudad varchar(150) NOT NULL,
-                province varchar(150) NOT NULL,
-                message varchar(550) NOT NULL,
-                direccion varchar(150) NOT NULL,
-                enProceso tinyint(1) NOT NULL DEFAULT 0,
-                enviado tinyint(1) NOT NULL DEFAULT 0,
-                pagoRealizado tinyint(1) NOT NULL DEFAULT 0,
-                fechaEntrega date NOT NULL,
-                id_venta varchar(150) NOT NULL DEFAULT 'null',
+                phone int(11) NOT NULL,
+                googleLocation varchar(150) NOT NULL,
                 nonce varchar(50) NOT NULL,
-                fecha timestamp NOT NULL DEFAULT current_timestamp(),
-                precio float(10,2) NOT NULL,
-                express varchar(3) NOT NULL,
+                currentDate timestamp NOT NULL DEFAULT current_timestamp(),
+                price float(10,2) NOT NULL,
                 PRIMARY KEY  (id)
             ) $charset_collate;";
          
@@ -51,7 +43,9 @@ function createAllTables(){
 }
 
 function removeAllTables(){
-
+    $optionsToDelette = [ 
+        "ctp_registered"
+    ];
 
     global $wpdb; 
 
@@ -60,6 +54,12 @@ function removeAllTables(){
     try {
         $removal_pluginDatabase = "DROP TABLE IF EXISTS {$table_plugin}";
         $remResult2 = $wpdb->query( $removal_pluginDatabase );
+
+        foreach ($optionsToDelette as $options_value) {
+            if (get_option($options_value)) {
+                delete_option($options_value);
+            } 
+        }
 
         return $remResult2;
     } catch (\Throwable $erro) {
